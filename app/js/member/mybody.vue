f01ba8f8da93eea36b4d1203234093db41995c64
notverysalty <1197181269@qq.com>
2019-05-30 21:22:19 +0800

    debug

--- app/js/member/mybody.vue
+++ app/js/member/mybody.vue
@@ -11,7 +11,7 @@
       </div>
       <div class="benifit">
         <span>{{ item.benifit }}</span>
-        <img src="https://m.jr.jd.com/spe/qyy/main/images/icon_arrow1.png" alt="">
+        <img src="//m.jr.jd.com/spe/qyy/main/images/icon_arrow1.png" alt="">
       </div>
       
     </div> -->
@@ -27,42 +27,42 @@
         items: [
           {
             id: 1,
-            src: 'https://img12.360buyimg.com/jrpmobile/jfs/t8971/199/1863233880/2035/56d04776/59cb5fbdNa7c682c3.png?width=70&height=70',
+            src: '//img12.360buyimg.com/jrpmobile/jfs/t8971/199/1863233880/2035/56d04776/59cb5fbdNa7c682c3.png?width=70&height=70',
             name: '我的资产',
             benifit: '查看全部资产'
           }, {
             id: 2,
-            src: 'https://img12.360buyimg.com/jrpmobile/jfs/t8836/18/2342020923/2120/a9b7a8ff/59cb6124N31cb2889.png?width=70&height=70',
+            src: '//img12.360buyimg.com/jrpmobile/jfs/t8836/18/2342020923/2120/a9b7a8ff/59cb6124N31cb2889.png?width=70&height=70',
             name: '我的优惠券',
             benifit: '查看全部'
           }, {
             id: 3,
-            src: 'https://img12.360buyimg.com/jrpmobile/jfs/t10264/48/2521964269/2177/cb479501/59f945edNc1197b64.png?width=90&height=90',
+            src: '//img12.360buyimg.com/jrpmobile/jfs/t10264/48/2521964269/2177/cb479501/59f945edNc1197b64.png?width=90&height=90',
             name: '我的金币',
             benifit: '赚金币兑好礼'
           }, {
             id: 4,
-            src: 'https://img12.360buyimg.com/jrpmobile/jfs/t12115/250/469950434/3906/ad97c994/5a0bdf3aN22efb724.png?width=90&height=90',
+            src: '//img12.360buyimg.com/jrpmobile/jfs/t12115/250/469950434/3906/ad97c994/5a0bdf3aN22efb724.png?width=90&height=90',
             name: '我的金条',
             benifit: '查看额度'
           }, {
             id: 5,
-            src: 'https://img12.360buyimg.com/jrpmobile/jfs/t9244/132/2364556535/1134/334cd5d9/59cb612dN5b974dc5.png?width=70&height=70',
+            src: '//img12.360buyimg.com/jrpmobile/jfs/t9244/132/2364556535/1134/334cd5d9/59cb612dN5b974dc5.png?width=70&height=70',
             name: '我的借钱',
             benifit: '查看全部产品'
           }, {
             id: 6,
-            src: 'https://img12.360buyimg.com/jrpmobile/jfs/t9121/110/2300703915/1348/91af205c/59cb613bN1e749e58.png?width=70&height=70',
+            src: '//img12.360buyimg.com/jrpmobile/jfs/t9121/110/2300703915/1348/91af205c/59cb613bN1e749e58.png?width=70&height=70',
             name: '我的白条',
             benifit: '去提额'
           }, {
             id: 7,
-            src: 'https://img12.360buyimg.com/jrpmobile/jfs/t1/18738/31/15049/5408/5cadb703E039f2599/e92ce5b31d8b1ecf.png?width=70&height=70',
+            src: '//img12.360buyimg.com/jrpmobile/jfs/t1/18738/31/15049/5408/5cadb703E039f2599/e92ce5b31d8b1ecf.png?width=70&height=70',
             name: '兑换优惠券',
             benifit: '去兑换'
           }, {
             id: 8,
-            src: 'https://img12.360buyimg.com/jrpmobile/jfs/t8014/149/2344686222/2102/341ff316/59cb616bN3e1bd87d.png?width=70&height=70',
+            src: '//img12.360buyimg.com/jrpmobile/jfs/t8014/149/2344686222/2102/341ff316/59cb616bN3e1bd87d.png?width=70&height=70',
             name: '我的保障',
             benifit: '全方位保障'
           }
